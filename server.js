d4b04d035774f87f35eb94dce31307e86890d2b7
André Teixeira <andre.teixeira@gmail.com>
2016-10-31 16:32:00 -0200

    Changing the port configuration on server.js to make the app executable on Heroku

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('<center><h1>Hello Express! 💛</h1></center>');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('MERN stack app listening on port 3000!');
 });
