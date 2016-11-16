47013dbdb4d6890b302a1f50110d4777c04c0195
André Teixeira <andre.teixeira@gmail.com>
2016-11-16 17:38:35 -0200

    Organization of server.js, extracting configuration to config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,32 @@
-const config = {
-  port: process.env.PORT || 3000,
-  mongo_url: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-stack',
-};
+const path = require('path')
+const express = require('express')
+const exphbs  = require('express-handlebars');
 
-module.exports = config;
+const mongoose = require('mongoose');
+
+module.exports = {
+  app: function () {
+    const app = express()
+    const publicPath = express.static(path.join(__dirname, 'public'))
+
+    // Defining assets configuration
+    app.use('/static', publicPath)
+
+    // Templates configuration
+	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+	app.set('view engine', 'handlebars');
+
+    return app
+  },
+
+  conn_mongo: function () {
+  	const mongo_url = process.env.MONGO_URL || 'mongodb://localhost:27017/mern-stack'
+
+  	mongoose.connect(mongo_url, (error) => {
+	  if (error) {
+	    console.error('Please verify if Mongodb is up and running!');
+	    throw error;
+	  }
+	});
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-var path = require('path');
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var mongoose = require('mongoose');
+const server_config = require('./config.js');
+const port = process.env.PORT || 3000;
+const app = server_config.app();
+const conn_mongo = server_config.conn_mongo();
 
-var app = express();
-
-var serverConfig = require('./config');
-
-// Connection to MongoDB
-mongoose.connect(serverConfig.mongo_url, (error) => {
-  if (error) {
-    console.error('Please verify if Mongodb is up and running!');
-    throw error;
-  }
-});
-
-// Serve public assets and static content
-app.use('/static', express.static(path.resolve(__dirname, '../build')));
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+console.log(conn_mongo);
 
 require('./routes/routes')(app);
 
-app.listen(serverConfig.port, function () {
-  console.log('MERN stack app listening on port ' + serverConfig.port + '!');
+// Development configuration
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('../webpack.dev.config.js')
+  const compiler = webpack(config)
+
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: false,
+    publicPath: config.output.publicPath
+  }))
+}
+
+// Start the application
+app.listen(port, function () {
+  console.log('MERN stack app listening on port ' + server_config.port + '!');
 });
