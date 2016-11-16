b6e47d080f3fd3d1ae54de7d183dd511e07be37e
André Teixeira <andre.teixeira@gmail.com>
2016-11-16 17:38:05 -0200

    Removing old webpack.config.js to create separate prod and dev config files

---
+++ webpack.prod.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const webpack = require('webpack');
+
+var webpackConfig = {
+    devtool: 'source-map',
+
+    entry: [
+        "./client/app.js",
+    ],
+
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{ test: /\.scss?$/,
+                loader: 'style!css!sass',
+                include: path.join(__dirname, 'src', 'styles') 
+            },{ test: /\.png$/,
+                loader: 'file' 
+            },{ test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file'
+            }
+        ]
+    }
+};
+
+module.exports = webpackConfig;
