0874ccdecbe6185b744147f05e6c2200dab159f0
André Teixeira <andre.teixeira@gmail.com>
2016-11-20 13:12:03 -0200

    Configuration of sass loader and css inclusion on template

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var webpackConfig = {
     devtool: 'eval',
@@ -17,7 +18,8 @@ var webpackConfig = {
 
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin("style.css")
     ],
 
     module: {
@@ -26,15 +28,20 @@ var webpackConfig = {
                 loader: "babel-loader",
                 test: /\.js$/,
                 exclude: /node_modules/
-            },{ test: /\.(css)?$/,
-                loader: 'style!css!sass',
-                include: path.join(__dirname, 'src', 'styles') 
-            },{ test: /\.png$/,
+            },{ 
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            },{ 
+                test: /\.png$/,
                 loader: 'file' 
-            },{ test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+            },{ 
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
                 loader: 'file'
             }
         ]
+    },
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, "./src/styles")]
     }
 };
 
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var webpackConfig = {
+const webpackConfig = {
     devtool: 'source-map',
 
     entry: [
@@ -24,7 +25,8 @@ var webpackConfig = {
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
-        })
+        }),
+        new ExtractTextPlugin("style.css")
     ],
 
     module: {
@@ -33,15 +35,20 @@ var webpackConfig = {
                 loader: "babel-loader",
                 test: /\.js$/,
                 exclude: /node_modules/
-            },{ test: /\.scss?$/,
-                loader: 'style!css!sass',
-                include: path.join(__dirname, 'src', 'styles') 
-            },{ test: /\.png$/,
+            },{ 
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            },{ 
+                test: /\.png$/,
                 loader: 'file' 
-            },{ test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+            },{ 
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
                 loader: 'file'
             }
         ]
+    },
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, "./src/styles")]
     }
 };
 
