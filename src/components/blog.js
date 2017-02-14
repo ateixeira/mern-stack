54978f6aec4312dde9e1d3024e09c9bf39dfb50a
Andre Teixeira <andre.teixeira@gmail.com>
2017-02-14 00:43:50 -0200

    Basic layout for the new BLOG app. It will be used as basis for the redux boilerplate implementation.

--- src/components/blog.js
+++ src/components/blog.js
@@ -7,7 +7,21 @@ module.exports = React.createClass({
         
         return (
             <div className="blog">
-                BLOG
+            	<div className="box">
+            		<div className="title">
+                		Lorem Ipsum Sit Amet Dolor Bla
+            		</div>
+            		<div className="content">
+                		Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Quem num gosti di mum que vai caçá sua turmis! Diuretics paradis num copo é motivis de denguis. Interagi no mé, cursus quis, vehicula ac nisi.
+
+						in elementis mé pra quem é amistosis quis leo. A ordem dos tratores não altera o pão duris Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Per aumento de cachacis, eu reclamis.
+
+						Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Cevadis im ampola pa arma uma pindureta. Leite de capivaris, leite de mula manquis.
+            		</div>
+            		<div className="date">
+            			13/02/2017 - 01:01PM
+            		</div>
+            	</div>
             </div>
         );
     }
