e2084c8ea80647b32f79f4753dc4a4cef6ba0121
Andre Teixeira <andre.teixeira@gmail.com>
2017-02-15 01:49:45 -0200

    Changing router to include a Redux provider

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
+// Import css
 import Style from './styles/style.scss';
 
+//Import Components
 import App from './components/app.js';
 import Blog from './components/blog.js';
 import Dashboard from './components/dashboard.js';
 import Cv from './components/cv.js';
 import Home from './components/home.js';
 
+//Import React Router and it's dependencies
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      {/* make them children of `App` */}
-      <IndexRoute component={Home}></IndexRoute>
-      <Route path="/blog" component={Blog}/>
-      <Route path="/dashboard" component={Dashboard}/>
-      <Route path="/cv" component={Cv}/>
-    </Route>
-  </Router>
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				{/* make them children of `App` */}
+				<IndexRoute component={Home}></IndexRoute>
+				<Route path="/blog" component={Blog}/>
+				<Route path="/dashboard" component={Dashboard}/>
+				<Route path="/cv" component={Cv}/>
+			</Route>
+		</Router>
+	</Provider>
 ), document.getElementById('container'))
