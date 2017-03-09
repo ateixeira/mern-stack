c7eefd1f4e81f2e4755728f4a4ccdc3cec2487eb
Andre Teixeira <andre.teixeira@gmail.com>
2017-03-09 15:00:35 -0300

    Adjusting server.js indentation to 4 spaces and cleaning the routes file to prepare the usage of a single router to both client and server

--- server/server.js
+++ server/server.js
@@ -3,24 +3,26 @@ const port = process.env.PORT || 3000;
 const app = server_config.app();
 const conn_mongo = server_config.conn_mongo();
 
-require('./routes/routes')(app);
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 // Development configuration
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('../webpack.dev.config.js')
-  const compiler = webpack(config)
+    const webpack = require('webpack')
+    const webpackDevMiddleware = require('webpack-dev-middleware')
+    const webpackHotMiddleware = require('webpack-hot-middleware')
+    const config = require('../webpack.dev.config.js')
+    const compiler = webpack(config)
 
-  app.use(webpackHotMiddleware(compiler))
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }))
+    app.use(webpackHotMiddleware(compiler))
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }))
 }
 
 // Start the application
 app.listen(port, function () {
-  console.log('MERN stack app listening on port ' + port + '!');
+    console.log('MERN stack app listening on port ' + port + '!');
 });
