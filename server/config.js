091625d4e34bb56d4a8fc14eed2b73f5bffd687a
Andre Teixeira <andre.teixeira@gmail.com>
2017-03-09 14:50:58 -0300

    Indentation on config.js

--- server/config.js
+++ server/config.js
@@ -5,28 +5,27 @@ const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
 
 module.exports = {
-  app: function () {
-    const app = express()
-    const publicPath = express.static(path.resolve(__dirname, '../public'))
+    app: function () {
+        const app = express()
+        const publicPath = express.static(path.resolve(__dirname, '../public'))
 
-    // Defining assets configuration
-    app.use('/public', publicPath)
+        // Defining assets configuration
+        app.use('/public', publicPath)
 
-    // Templates configuration
-	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-	app.set('view engine', 'handlebars');
+        // Templates configuration
+        app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
 
-    return app
-  },
+        return app
+    },
 
-  conn_mongo: function () {
-  	const mongo_url = process.env.MONGO_URL || 'mongodb://localhost:27017/mern-stack'
+    conn_mongo: function () {
+        const mongo_url = process.env.MONGO_URL || 'mongodb://localhost:27017/mern-stack'
 
-  	mongoose.connect(mongo_url, (error) => {
-	  if (error) {
-	    console.error('Please verify if Mongodb is up and running!');
-	    // throw error;
-	  }
-	});
-  }
+        mongoose.connect(mongo_url, (error) => {
+            if (error) {
+                console.error('Please verify if Mongodb is up and running!');
+            }
+        });
+    }
 }
